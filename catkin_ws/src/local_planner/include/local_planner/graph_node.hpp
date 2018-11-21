0c8c969a262fca957bfb55aa846116fbf7ef9d8e
John Blankenhorn <jrblankenhorn@wpi.edu>
2018-11-20 20:37:28 -0500

    forgot to add this hehe

---
+++ catkin_ws/src/local_planner/include/local_planner/graph_node.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+typedef std::pair<double, double> point;
+
+namespace Prius {
+
+struct GraphNode{
+
+    point child_point;
+    point parent_point;
+    double heading;
+    double velocity;
+    double g = 0;
+    double cost;
+
+    GraphNode(const point &cp, const point &pp, const double &h, const double &v)
+    {
+        child_point = cp;
+        parent_point = pp;
+        heading = h;
+        velocity = v;
+
+    }
+
+    bool operator==(const GraphNode &rhs) const
+    {
+        return child_point == rhs.child_point &&
+               parent_point == rhs.parent_point &&
+               heading == rhs.heading &&
+               velocity == rhs.velocity;
+    }
+};
+
+}
